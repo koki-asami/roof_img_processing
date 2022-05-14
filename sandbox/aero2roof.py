7c424d0848afd3f2ef316528493986a7a848307d
kokiasami <is0392hr@gmail.com>
2022-05-14 17:28:31 +0900

    Update aero2roof.py

--- sandbox/aero2roof.py
+++ sandbox/aero2roof.py
@@ -24,7 +24,7 @@ IMG_HIGHT = 7500
 
 DATA_DIR = Path('./')
 
-COORD_FILE = "c02KD67120220401建築物_Merged_xy.csv"
+COORD_FILE = 'Export_output.csv'  # "c02KD67120220401建築物_Merged_xy.csv"
 COORD_FILE_PATH = DATA_DIR / COORD_FILE
 
 AERO_IMG_DIR = DATA_DIR / "10cm高解像度"
@@ -70,11 +70,22 @@ def open_image(path):
 # ar, ag, ab, br, bg, bb, cr, cg, cb, dr, dg, db = [], [], [], [], [], [], [], [], [],[],[],[]
 # print("finish")
 
-def koten(apx, apy, bpx, bpy, cpx, cpy, dpx, dpy): # intersection
+def intersection(apx, apy, bpx, bpy, cpx, cpy, dpx, dpy): # intersection
     s1 = ((apx - cpx) * (bpy - cpy)) + ((bpx - cpx) * (cpy - apy))
     s2 = ((apx - dpx) * (bpy - dpy)) + ((bpx - dpx) * (dpy - apy))
     return s1 * s2
 
+def lat2pix(lat, lat_up, lat_down):
+    return (lat_up - lat) / (lat_up - lat_down) * IMG_HIGHT
+
+def lon2pix(lon, lon_l, lon_r):
+    return (lon - lon_l) / (lon_r - lon_l) * IMG_WIDTH
+
+
+def koten(apx, apy, bpx, bpy, cpx, cpy, dpx, dpy): # intersection
+    s1 = ((apx - cpx) * (bpy - cpy)) + ((bpx - cpx) * (cpy - apy))
+    s2 = ((apx - dpx) * (bpy - dpy)) + ((bpx - dpx) * (dpy - apy))
+    return s1 * s2
 def idotopix(ido,upido,downido):
     return (upido-ido)/(upido-downido)*7500
 def keidotopix(keido,leftkeido,rightkeido):
@@ -87,24 +98,32 @@ location_point_of_image3 = [-15999.9, -13999.9, -21000.1, -19500.1]
 location_point_of_image4 = [-13999.9, -11999.9, -21000.1, -19500.1]
 location_point_list = np.asarray([location_point_of_image1, location_point_of_image2, location_point_of_image3, location_point_of_image4])
 
-def image_judge(location_point_list, pi, pk):
-    judge_array = np.ones((5))
-    for n in range(0,len(pi)):
-        point_judge = 4
-        for m in range(0, len(location_point_list)):
-            if location_point_list[m, 0] < pk[n] < location_point_list[m, 1] and location_point_list[m, 2] < pi[n] < location_point_list[m, 3]:
-                point_judge = m
-        judge_array[point_judge] = judge_array[point_judge] * 0
-    judge = 4
-    if (judge_array == np.asarray([0,1,1,1,1])).all():
-        judge = 0
-    elif (judge_array == np.asarray([1,0,1,1,1])).all():
-        judge = 1
-    elif (judge_array == np.asarray([1,1,0,1,1])).all():
-        judge = 2
-    elif (judge_array == np.asarray([1,1,1,0,1])).all():
-        judge = 3
-    return judge
+# def image_judge(location_point_list, pi, pk):
+#     judge_array = np.ones((5))
+#     for n in range(0,len(pi)):
+#         point_judge = 4
+#         for m in range(0, len(location_point_list)):
+#             if location_point_list[m, 0] < pk[n] < location_point_list[m, 1] and location_point_list[m, 2] < pi[n] < location_point_list[m, 3]:
+#                 point_judge = m
+#         judge_array[point_judge] = judge_array[point_judge] * 0
+#     judge = 4
+#     if (judge_array == np.asarray([0,1,1,1,1])).all():
+#         judge = 0
+#     elif (judge_array == np.asarray([1,0,1,1,1])).all():
+#         judge = 1
+#     elif (judge_array == np.asarray([1,1,0,1,1])).all():
+#         judge = 2
+#     elif (judge_array == np.asarray([1,1,1,0,1])).all():
+#         judge = 3
+#     return judge
+
+
+def image_judge(location_points, polygon):
+    flag = False
+    for lon, lat in polygon:
+        if location_points[0] < lon < location_points[1] and location_points[2] < lat < location_points[3]:
+            flag = True
+    return flag
 
 def make_circle_list(listpk, listpi):
     circle_list = []
@@ -135,31 +154,74 @@ def make_circle_list(listpk, listpi):
                 e = []
     return circle_list
 
-def imaging(hr, hg, hb, listpk, listpi, x1, n):
-    minkeidop = min(listpk)
-    maxkeidop = max(listpk)
-    minidop = min(listpi)
-    maxidop = max(listpi)
+# def imaging(id, img, listpk, listpi, n=0):
+#     hr, hg, hb = img
+#     minkeidop = min(listpk)
+#     maxkeidop = max(listpk)
+#     minidop = min(listpi)
+#     maxidop = max(listpi)
+#     imagelistr = []
+#     imagelistg = []
+#     imagelistb = []
+#     imgyoko = maxkeidop - minkeidop + 1
+#     imgtate = maxidop - minidop + 1
+#     circle_lisit = make_circle_list(listpk, listpi)
+#     for x3 in range(minidop, maxidop + 1):
+#         for x4 in range(minkeidop, maxkeidop + 1):
+#             x3 = x3 + 0.5
+#             majiwari = 0
+#             for c in circle_lisit:
+#                 for n in range(0, len(c) - 1):
+#                     if koten(x4, x3, maxkeidop, x3, listpk[c[n]], listpi[c[n]], listpk[c[n + 1]], listpi[c[n + 1]]) <= 0 and koten(
+#                             listpk[c[n]], listpi[c[n]], listpk[c[n + 1]], listpi[c[n + 1]], x4, x3, maxkeidop, x3) <= 0:
+#                         majiwari = majiwari + 1
+#             x3 = int(x3 - 0.5)
+#             if majiwari % 2 == 1 :
+#                 imagelistr.append(hr[x3, x4])
+#                 imagelistg.append(hg[x3, x4])
+#                 imagelistb.append(hb[x3, x4])
+#             else:
+#                 imagelistr.append(0)
+#                 imagelistg.append(0)
+#                 imagelistb.append(0)
+#     imagelistr11 = np.asarray(imagelistr)
+#     imagelistg11 = np.asarray(imagelistg)
+#     imagelistb11 = np.asarray(imagelistb)
+#     imagelistr2 = np.reshape(imagelistr11, (imgtate, imgyoko))
+#     imagelistg2 = np.reshape(imagelistg11, (imgtate, imgyoko))
+#     imagelistb2 = np.reshape(imagelistb11, (imgtate, imgyoko))
+#     img = Image.new('RGB', (imgyoko, imgtate), (255, 255, 255))
+#     for y in range(0, imgyoko):
+#         for x in range(0, imgtate):
+#             img.putpixel((y, x), (imagelistr2[x, y], imagelistg2[x, y], imagelistb2[x, y]))
+#     img_resize = img.resize((227, 227), resample=Image.BICUBIC)
+#     img_resize.save('F5/' + str(id) + "_" + str(n) + '.bmp')
+
+def imaging(id, img, polygon_x, polygon_y):
+    lon_min = min(polygon_x)
+    lon_max = max(polygon_x)
+    lat_min = min(polygon_y)
+    lat_max = max(polygon_y)
     imagelistr = []
     imagelistg = []
     imagelistb = []
-    imgyoko = maxkeidop - minkeidop + 1
-    imgtate = maxidop - minidop + 1
-    circle_lisit = make_circle_list(listpk, listpi)
-    for x3 in range(minidop, maxidop + 1):
-        for x4 in range(minkeidop, maxkeidop + 1):
+    img_w = lon_max - lon_min + 1
+    img_h = lat_max - lat_min + 1
+    circle_lisit = make_circle_list(polygon_x, polygon_y)
+    for x3 in range(lat_min, lat_max + 1):
+        for x4 in range(lon_min, lon_max + 1):
             x3 = x3 + 0.5
             majiwari = 0
             for c in circle_lisit:
                 for n in range(0, len(c) - 1):
-                    if koten(x4, x3, maxkeidop, x3, listpk[c[n]], listpi[c[n]], listpk[c[n + 1]], listpi[c[n + 1]]) <= 0 and koten(
-                            listpk[c[n]], listpi[c[n]], listpk[c[n + 1]], listpi[c[n + 1]], x4, x3, maxkeidop, x3) <= 0:
+                    if koten(x4, x3, lon_max, x3, polygon_x[c[n]], polygon_y[c[n]], polygon_x[c[n + 1]], polygon_y[c[n + 1]]) <= 0 and koten(
+                            polygon_x[c[n]], polygon_y[c[n]], polygon_x[c[n + 1]], polygon_y[c[n + 1]], x4, x3, lon_max, x3) <= 0:
                         majiwari = majiwari + 1
             x3 = int(x3 - 0.5)
             if majiwari % 2 == 1 :
-                imagelistr.append(hr[x3, x4])
-                imagelistg.append(hg[x3, x4])
-                imagelistb.append(hb[x3, x4])
+                imagelistr.append(img[0][x3, x4])
+                imagelistg.append(img[1][x3, x4])
+                imagelistb.append(img[2][x3, x4])
             else:
                 imagelistr.append(0)
                 imagelistg.append(0)
@@ -167,15 +229,15 @@ def imaging(hr, hg, hb, listpk, listpi, x1, n):
     imagelistr11 = np.asarray(imagelistr)
     imagelistg11 = np.asarray(imagelistg)
     imagelistb11 = np.asarray(imagelistb)
-    imagelistr2 = np.reshape(imagelistr11, (imgtate, imgyoko))
-    imagelistg2 = np.reshape(imagelistg11, (imgtate, imgyoko))
-    imagelistb2 = np.reshape(imagelistb11, (imgtate, imgyoko))
-    img = Image.new('RGB', (imgyoko, imgtate), (255, 255, 255))
-    for y in range(0, imgyoko):
-        for x in range(0, imgtate):
+    imagelistr2 = np.reshape(imagelistr11, (img_h, img_w))
+    imagelistg2 = np.reshape(imagelistg11, (img_h, img_w))
+    imagelistb2 = np.reshape(imagelistb11, (img_h, img_w))
+    img = Image.new('RGB', (img_w, img_h), (255, 255, 255))
+    for y in range(0, img_w):
+        for x in range(0, img_h):
             img.putpixel((y, x), (imagelistr2[x, y], imagelistg2[x, y], imagelistb2[x, y]))
     img_resize = img.resize((227, 227), resample=Image.BICUBIC)
-    img_resize.save('F5/mashiki_kiban' + str(x1) + "_" + str(n) + '.bmp')
+    img_resize.save('F5/' + str(id) + "_" + str(n) + '.bmp')
 
 def delete_list(h, del_axis):
     for i in range(0,len(del_axis)):
@@ -209,18 +271,27 @@ def delete_list(h, del_axis):
 def main():
 
     # Load polygon coordinates as DATAFRAME
-    df = pd.read_csv(COORD_FILE_PATH, usecols=[1,2,3])
+    df = pd.read_csv(COORD_FILE_PATH, usecols=[0,1,2])  # [1,2,3])
     # Exstract unique building_id from df
     ids = df.drop_duplicates(subset = ['id'])['id'].to_list()
-
-    for img_path in sorted(AERO_IMG_DIR.glob('*.jpg')):
+    imgs = ['20160417-02KD664-a20.jpg']
+    for img_path in imgs: # sorted(AERO_IMG_DIR.glob('*.jpg')):
         print(img_path)
-        jgw_path = img_path.with_suffix('.jgw')
-        location_point = get_location_point(jgw_path)
+        jgw_path = Path(img_path).with_suffix('.jgw')
+        location_points = get_location_point(jgw_path)
         img = open_image(str(img_path))
-        for id in ids[:2]:
+        for id in ids:
             selected_df = df.loc[df['id']==id]
             polygon = [[float(row['POINT_X']), float(row['POINT_Y'])] for idx, row in selected_df.iterrows()]
+
+            if image_judge(location_points, polygon):  # judge whether the selected polygon is in the target image
+                # convert coordinates of polygon from lonlat to pixel
+                polygon_x = [int(lon2pix(p[0], location_points[0], location_points[1])) for p in polygon]
+                polygon_y = [int(lat2pix(p[1], location_points[3], location_points[2])) for p in polygon]
+                
+                # crop roof image from aerial photo
+                imaging(id, img, polygon_x, polygon_y)
+            
 
 if __name__ == '__main__':
     main()
